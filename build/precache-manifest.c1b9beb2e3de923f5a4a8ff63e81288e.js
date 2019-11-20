c51b88a6489e3e0737299e54a4510d6447082ca2
Brian Icaro D. Marques <bi.dm@hotmail.com>
2019-11-20 14:08:25 -0300

    first commit to the react version

---
+++ build/precache-manifest.c1b9beb2e3de923f5a4a8ff63e81288e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbf85a05b8ef587cfdc87c8d1056a48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d347eb62fd6a595dd3da",
+    "url": "/static/css/main.6bdfde95.chunk.css"
+  },
+  {
+    "revision": "841ecadf7e1420866dc5",
+    "url": "/static/js/2.2de8f381.chunk.js"
+  },
+  {
+    "revision": "d347eb62fd6a595dd3da",
+    "url": "/static/js/main.01371824.chunk.js"
+  },
+  {
+    "revision": "6a2ffb6b60deef551637",
+    "url": "/static/js/runtime-main.441c1036.js"
+  }
+]);
